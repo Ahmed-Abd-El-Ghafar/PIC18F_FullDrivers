296ea72ccdd2f09be9d919ef4145468f647c602b
Ahmed Abd El-Ghafar Mohammed <ahmed.batra2011@gmail.com>
2019-12-07 17:04:55 +0200

    Update seven_segment.c

--- ECU/seven_segment.c
+++ ECU/seven_segment.c
@@ -4,6 +4,12 @@
 uint8_t seg_values_a[10] = {192, 249, 164, 176, 153,
                             146, 130, 248, 128, 144};
 
+/**
+  * @brief   
+  * @param  
+  * @param 
+  * @retval 
+  */
 void write_digit_seven_segment(Register_t *reg, uint8_t val){
     write_value_to_port(reg, val);
 }
